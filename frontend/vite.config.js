2c1d3d8dbdf1e30d8f027a9c46292cffd55b156a
Dinesh <153713000+Dinesh-Sharma2004@users.noreply.github.com>
2025-12-03 21:20:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/",
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5000,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000', // backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: "/",   // required for Render deployment
 })
